6e6f58e7d4c1ab63ce305f1090db7439ba06aa88
Hamza <m.hamza.arif786@gmail.com>
2019-11-07 14:51:27 +0500

    User Role

--- ProjectManagement/Controllers/AccountController.cs
+++ ProjectManagement/Controllers/AccountController.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using ProjectManagement.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Web.Security;
 
 namespace ProjectManagement.Controllers
 {
@@ -151,11 +153,16 @@ namespace ProjectManagement.Controllers
         {
             if (ModelState.IsValid)
             {
+                
                 var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
+                    var _context = new ApplicationDbContext();
+                    var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(_context));
+                    UserManager.AddToRole(user.Id, "User");
+
+                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);                    
                     
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
